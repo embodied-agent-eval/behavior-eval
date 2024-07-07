d3858ee3d66bf5b4e8e50f09e5486fb49ab89407
Kangrui Wang <krwang2022@gmail.com>
2024-07-07 03:29:42 -0500

    temply commented out some modules in main

--- behavior_eval/main.py
+++ behavior_eval/main.py
@@ -2,9 +2,9 @@ import fire
 from typing import Optional
 from behavior_eval.evaluation.action_sequence.scripts.evaluate_results import evaluate_results as action_sequence_evaluate_results
 from behavior_eval.evaluation.action_sequence.scripts.generate_prompts import generate_prompts as action_sequence_generate_prompts
-from behavior_eval.evaluation.goal_interpretation.scripts.evaluate_results import evaluate_results as goal_interpretation_evaluate_results
-from behavior_eval.evaluation.goal_interpretation.scripts.generate_prompts import generate_prompts as goal_interpretation_generate_prompts
-from behavior_eval.evaluation.subgoal_decomposition.scripts.generate_prompts import generate_prompts as subgoal_decomposition_generate_prompts
+# from behavior_eval.evaluation.goal_interpretation.scripts.evaluate_results import evaluate_results as goal_interpretation_evaluate_results
+# from behavior_eval.evaluation.goal_interpretation.scripts.generate_prompts import generate_prompts as goal_interpretation_generate_prompts
+# from behavior_eval.evaluation.subgoal_decomposition.scripts.generate_prompts import generate_prompts as subgoal_decomposition_generate_prompts
 import os
 def main(module:Optional[str]="action_sequence",func:Optional[str]="generate_prompts",worker_num:Optional[int]=1,llm_response_dir:Optional[str]=None,result_dir:Optional[str]='./results'):
     """
@@ -28,17 +28,17 @@ def main(module:Optional[str]="action_sequence",func:Optional[str]="generate_pro
             action_sequence_evaluate_results(llm_response_dir,worker_num,result_dir)
         elif func == "generate_prompts":
             action_sequence_generate_prompts(worker_num,result_dir)
-    elif module == "goal_interpretation":
-        if func == "evaluate_results":
-            goal_interpretation_evaluate_results(llm_response_dir)
-        elif func == "generate_prompts":
-            goal_interpretation_generate_prompts()
-    elif module == "subgoal_decomposition":
-        if func == "evaluate_results":
-            raise NotImplementedError("This part is yet to be implemented")
-        elif func == "generate_prompts":
-            worker_num = worker_num if worker_num else 1
-            subgoal_decomposition_generate_prompts(worker_num)
+    # elif module == "goal_interpretation":
+    #     if func == "evaluate_results":
+    #         goal_interpretation_evaluate_results(llm_response_dir)
+    #     elif func == "generate_prompts":
+    #         goal_interpretation_generate_prompts()
+    # elif module == "subgoal_decomposition":
+    #     if func == "evaluate_results":
+    #         raise NotImplementedError("This part is yet to be implemented")
+    #     elif func == "generate_prompts":
+    #         worker_num = worker_num if worker_num else 1
+    #         subgoal_decomposition_generate_prompts(worker_num)
 
 
 if __name__ == '__main__':
