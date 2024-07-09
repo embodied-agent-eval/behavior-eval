b790fd93be162f4e4fbcc676a4c2503cfc1c8c4a
Kangrui Wang <krwang2022@gmail.com>
2024-07-09 14:49:24 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,57 @@
-# coding:utf-8
-
-from setuptools import find_packages, setup
-import os
 import subprocess
+import sys
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+def is_package_installed(package_name, version=None):
+    try:
+        import pkg_resources
+        if version:
+            pkg_resources.require(f"{package_name}=={version}")
+        else:
+            pkg_resources.require(package_name)
+        return True
+    except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict):
+        return False
+
+def install_package(package_name, url):
+    if not is_package_installed(package_name):
+        process = subprocess.Popen([sys.executable, "-m", "pip", "install", url],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   universal_newlines=True)
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                sys.stdout.write(output)
+                sys.stdout.flush()
+        
+        # Print any remaining stderr output
+        stderr = process.communicate()[1]
+        if stderr:
+            sys.stderr.write(stderr)
+            sys.stderr.flush()
+
+install_package("igibson", "git+https://github.com/embodied-agent-eval/iGibson.git@master#egg=igibson")
+install_package("bddl", "git+https://github.com/embodied-agent-eval/bddl.git@v1.0.2#egg=bddl")
 
 setup(
     name='behavior-eval',
-    version='1.0.1',
+    version='1.0.0',
     author='stanford',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/embodied-agent-eval/behavior-eval",
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=[
-        'igibson @ git+https://github.com/embodied-agent-eval/iGibson.git@master#egg=igibson',
-        'bddl @ git+https://github.com/embodied-agent-eval/bddl.git@v1.0.2#egg=bddl',
+        "fire",
+        "lark",
     ],
     include_package_data=True,
     package_data={
         'igibson': ['*.dll','*.pyd'],
-        '': ['*.json', '*.xml', '*.md'],
+        '': ['*.json', '*.xml', '*.md','*.yaml'],
     },
 )
