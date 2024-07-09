a418b1c5853662e1cba7592a87607ed743c55739
Kangrui Wang <krwang2022@gmail.com>
2024-07-08 22:03:10 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,6 @@ from setuptools import find_packages, setup
 import os
 import subprocess
 
-def install_dependency_if_missing(repo_url, package_name):
-    try:
-        __import__(package_name)
-    except ImportError:
-        subprocess.check_call(["pip", "install", f"git+{repo_url}#egg={package_name}"])
-
-install_dependency_if_missing('https://github.com/embodied-agent-eval/iGibson.git@master', 'igibson')
-install_dependency_if_missing('https://github.com/embodied-agent-eval/bddl.git@v1.0.2', 'bddl')
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -26,8 +17,8 @@ setup(
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=[
-        'igibson',
-        'bddl',
+        'igibson @ git+https://github.com/embodied-agent-eval/iGibson.git@master#egg=igibson',
+        'bddl @ git+https://github.com/embodied-agent-eval/bddl.git@v1.0.2#egg=bddl',
     ],
     include_package_data=True,
     package_data={
